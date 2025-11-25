0ed9627d5c233af2bb2031990b16de44a0963e48
McNuggetsIT <davanzogiuseppe1@outlook.it>
2025-11-25 17:49:50 +0100

    Add functions for column and row operations

--- moduli/multi_dim_operation.py
+++ moduli/multi_dim_operation.py
@@ -18,3 +18,25 @@ def normaMatrice(A):
 def covarianzaMatrice(At):
     transpose = transpostaMatrice(At)
     return np.cov(transpose)
+    
+def somma_colonne(matrix):
+    
+    sum1 = np.sum(matrix , axis = 0)
+    print("Somma per colonne:" , sum1)
+    
+def somma_righe(matrix):
+    sum2 = np.sum(matrix , axis = 1)
+    print("Somma per righe:" , sum2)
+    
+def media_colonne(matrix):
+    
+    mean1 = np.mean(matrix , axis = 0)
+    print("Media per colonne:", mean1)
+
+def media_righe(matrix):
+
+    mean2 = np.mean(matrix, axis = 1)
+    print("Media per righe:", mean2)
+    
+    
+    
